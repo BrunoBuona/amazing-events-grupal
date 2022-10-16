34b6d85ef2d178e6ee30c2abfebc93cf6298d716
jonyolmeda <jonyolmeda@gmail.com>
2022-10-16 02:13:57 -0300

    Test events js

--- script/index.js
+++ script/index.js
@@ -3,3 +3,21 @@ let input = document.getElementById(`search`)
 input.addEventListener(`change`, (e) => {
     console.log(e);
 })
+
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".articulo").forEach(fruta =>{
+  
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?fruta.classList.remove("filtro")
+              :fruta.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
