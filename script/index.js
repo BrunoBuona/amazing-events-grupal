c0a86df616aeefd802eec664b3adc954148a4e62
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-10-16 21:24:29 -0300

    Update 1.2

--- script/index.js
+++ script/index.js
@@ -1,5 +1,23 @@
-let input = document.getElementById(`search`)
+let cardsJs = document.getElementById("cards-js");
+const imprimir = events.forEach(e => { cardsJs.innerHTML += `
+            <div class="card">
+            <img src=${e.image} class="card-img-top" alt="Card">
+            <div class="card-body">
+            <h5 class="card-title">${e.name}</h5>
+            <p class="card-text">${e.description}</p>
+            <a href="#" class="btn btn-primary">See more</a>
+            </div>
+  `;
+});
 
-input.addEventListener(`change`, (e) => {
-    console.log(e);
-})
+
+// checksWCheck=[]
+// document.addEventListener("change", e=>{
+//   if(e.target.checked){
+//    checksWCheck = checksWCheck.concat(events.filter(categorys.includes(e.target.checked)))
+//   }if(!e.target.checked){
+
+//   }else{
+
+//   }
+// })
