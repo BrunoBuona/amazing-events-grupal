d2bf386da36592d83ebc1a0658ef377d1bb337c6
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-10-17 00:15:43 -0300

    v1.0.1 searchbar

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
 let cardsJs = document.getElementById("cards-js");
-const imprimir = events.forEach(e => { cardsJs.innerHTML += `
+let buscador = document.getElementById("buscador")
+let imprimir = events.forEach(e => {
+  cardsJs.innerHTML += `
             <div class="card">
             <img src=${e.image} class="card-img-top" alt="Card">
             <div class="card-body">
@@ -9,8 +11,25 @@ const imprimir = events.forEach(e => { cardsJs.innerHTML += `
             </div>
   `;
 });
+let arrayNames = events.map((names) => names.name.toLowerCase())
 
 
+let element = []
+buscador.addEventListener("keyup", e => {
+  elementFiltrados = arrayNames.filter(names => names.includes(e.target.value.toLowerCase()))
+  cardsJs.innerHTML = ''
+  elementFiltrados.forEach(e => {
+    cardsJs.innerHTML += `
+<div class="card">
+<img src=${e.image} class="card-img-top" alt="Card">
+<div class="card-body">
+<h5 class="card-title">${e.name}</h5>
+<p class="card-text">${e.description}</p>
+<a href="#" class="btn btn-primary">See more</a>
+</div>
+`;
+  });
+})
 // checksWCheck=[]
 // document.addEventListener("change", e=>{
 //   if(e.target.checked){
